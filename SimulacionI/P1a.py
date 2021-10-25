7431e0b8574c112dc0161cf2efb23f33bd701afe
esquintero <esquintero@unal.edu.co>
2021-10-25 18:26:32 -0500

    creacion del repo

---
+++ SimulacionI/P1a.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 25 16:19:40 2021
+
+@author: Esteban
+"""
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import random as rn
+
+fmax = 0.86
+x = range(13)
+m1 = fmax/8
+m2 = -fmax/4
+
+pdf = np.zeros(13)
+
+for i in range(13):
+    if i <= 8:
+        pdf[i] = m1*i
+    else:
+        pdf[i] = m2*i - m2*12
+        
+    
+
+plt.plot(x, pdf)
+plt.show()
