cfa9ffce7cc9e7e2a7199ea6c6f977078de2be3d
esquintero <esquintero@unal.edu.co>
2021-10-26 16:52:29 -0500

    Funcion distribucion acumulada

--- SimulacionI/P1a.py
+++ SimulacionI/P1a.py
@@ -11,20 +11,26 @@ import matplotlib.pyplot as plt
 import random as rn
 
 fmax = 1/6
-grid = np.zeros(13)
+grid = np.zeros(12)
 m1 = fmax/8
 m2 = -fmax/4
 
-pdf = np.zeros(13)
+pdf = np.zeros(12)
+F = np.zeros(12)
 
-for i in range(13):
+for i in range(12):
     grid[i] = 0.5 + i
-    if i <= 8:
-        pdf[i] = m1*i
+    if i <= 7:
+        pdf[i] = m1*grid[i]
+      
     else:
-        pdf[i] = m2*i - m2*12
-        
-    
+        pdf[i] = m2*grid[i] - m2*12
 
-plt.plot(grid, pdf)
+for j in range(11): 
+    F[j + 1] = F[j] + pdf[j + 1]        
+        
+
+plt.plot(grid, pdf, 'bo')
+plt.plot(grid, F, 'ro')
 plt.show()
+
