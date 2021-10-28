3d667703df6e1936bcdda1e1926ee56219472745
androjaslo <androjaslo@unal.edu.co>
2021-10-27 19:25:58 -0500

    Cambios a graficas

--- SimulacionI/P1a.py
+++ SimulacionI/P1a.py
@@ -18,6 +18,8 @@ m2 = -fmax/4
 pdf = np.zeros(12)
 F = np.zeros(12)
 
+#  Funcion densidad de probabilidad
+
 for i in range(12):
     grid[i] = 0.5 + i
     if i <= 7:
@@ -26,11 +28,25 @@ for i in range(12):
     else:
         pdf[i] = m2*grid[i] - m2*12
 
+        
+# Acumulativa
+
 for j in range(11): 
     F[j + 1] = F[j] + pdf[j + 1]        
-        
+       
 
+
+ax=plt.gca()
 plt.plot(grid, pdf, 'bo')
-plt.plot(grid, F, 'ro')
+plt.grid(True)
+
+plt.figure()
+plt.plot(grid, F, ds='steps-mid',c='r')
+ax=plt.gca()
+ax.set_xticks(range(0,12))
+plt.grid(True)
+
+
 plt.show()
+
 
