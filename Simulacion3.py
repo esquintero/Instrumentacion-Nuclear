9361eafdb2cd90d702c7a7522d09febf894580a3
androjaslo <androjaslo@unal.edu.co>
2021-12-15 17:23:10 -0500

    Queda sin decaer un nucleo

--- Simulacion3.py
+++ Simulacion3.py
@@ -10,6 +10,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import random as rn
 import math as math
+from scipy.stats import poisson
 
 #Parámetros correspondientes al Isotopo a trabajar
 #Nombre Isotopo : Carbono 14
@@ -22,22 +23,21 @@ dt=1.#tau/10.
 tf=3*tau
 num_intervalos=round(tf/dt)
 
-timegrid=np.arange(1,num_intervalos,dt)
+timegrid=np.arange(0,num_intervalos,dt)
 
 
 #Funcion que define probabilidad de Poisson
-def ProbPoisson(Gamma,N0,dt):
-    mu1=N0*Gamma*dt
+def ProbPoisson(N0,dt):
+    mu1=N0*Gamma*dt # Por definicion, ¿tendremos así mismo mu que va variando?
     mu2=N0*Gamma*30*dt #Por que 30?
-    prob_ini= np.zeros(N0) #Probabilidad de que x nucleos decaiga en un tiempo inicial
-    prob_inter=np.zeros(N0) #Probabilidad de que decaiga en tiempo intermedio
+    prob_ini= np.zeros(N0) 
+    prob_inter=np.zeros(N0)
     for i in range(N0):
-        prob_ini[i]=(np.power(mu1,i)*np.exp(-mu1))/np.math.factorial(i)
-        prob_inter[i]=(np.power(mu2,i)*np.exp(-mu2))/np.math.factorial(i)
+        prob_ini[i]=(np.power(mu1,i)*np.exp(-mu1))/np.math.factorial(i)#Probabilidad de que x nucleos decaiga en un tiempo inicial
+        prob_inter[i]=(np.power(mu2,i)*np.exp(-mu2))/np.math.factorial(i) #Probabilidad de que decaiga en tiempo intermedio
     return prob_ini,prob_inter
-
-ini,inter=ProbPoisson(Gamma,N0,dt)
-print(ini)
+muini=N0*Gamma*dt
+ini,inter=ProbPoisson(N0,dt)
 
 xnucleos=np.arange(0,N0,1)
 plt.figure()
@@ -51,12 +51,136 @@ plt.xticks(np.arange(0,100,10))
 plt.show()
 
 
+#Funcion acumulada
+"""
+def acumulada(N0,prob):
+    F=np.zeros(N0)
+    for j in range(99): 
+        F[j + 1] = F[j]   +prob[j+1]     
+    return F
+    
+#Finter=acumulada(N0,inter)
+#Fini=acumulada(N0,ini)    
+"""
+
+Finter=np.cumsum(inter)
+Fini=np.cumsum(ini)
+plt.figure()
+plt.plot(xnucleos,Finter,'r',label="Tiempo intermedio")
+plt.plot(xnucleos,Fini,'b',label="Tiempo inicial")
+plt.title("Probabilidad acumulada de decaimiento en 3*Tau siglos con tiempo intermedio")
+plt.xlabel("Nucleos")
+plt.ylabel("Probabilidad")
+plt.legend(loc=0)
+plt.show()
+
+#Actividad
+
+def actividad(N0,dt):
+    ENE=[N0]
+    A=[poisson.rvs(muini,size=1)]
+    N0=N0-np.sum(A)
+    
+    while N0>0:
+        ENE.append(N0)
+        mu=N0*Gamma*dt
+        A.append(poisson.rvs(mu,size=1))
+        N0=N0-A[-1]
+        #print(N0)
+        
+    for i in range(len(A)):
+        A[i]=A[i]/dt
+    return A,ENE
+
+A,ENE=actividad(N0,dt)
+plt.figure()
+print(np.sum(A))
+time=np.arange(0,len(A)*dt,dt)
+plt.plot(time,ENE,ds="steps",label="Actividad")
+plt.xlabel("Tiempo")
+plt.ylabel("Nucleos")
+#plt.yticks(np.arange(0,N0,10))
+
+#Probabilidad de poisson con pmf
+timegrid=np.arange(0,80,dt)
+probini=poisson.pmf(timegrid,muini)
+probinter=poisson.pmf(timegrid,muini*30)
+plt.figure()
+plt.plot(timegrid,probini)
+plt.plot(timegrid,probinter)
+plt.title("LA de")
+plt.show()
+
+plt.figure()
+cum0=np.cumsum(probini)
+cum1=np.cumsum(probinter)
+plt.plot(timegrid,cum0)
+plt.plot(timegrid,cum1)
+plt.show()
 
 
+N1=10
+N2=100
+N3=1000
+N4=10**6
 
 
+A1,ENE1=actividad(N1,dt)
+A2,ENE2=actividad(N2,dt)
+A3,ENE3=actividad(N3,dt)
+A4,ENE4=actividad(N4,dt)
+#print(np.sum(A1))
+time=np.arange(0,len(A1)*dt,dt)
 
+fig=plt.figure()
 
+y1=(N1*np.exp(-time*Gamma))
+plt.plot(time,ENE1,ds="steps",label="Actividad")
+plt.plot(time,y1,label="Ley de decaimiento")
+plt.title("Comparación para N=10")
+plt.xlabel("Tiempo")
+plt.legend(loc=0)
+plt.ylabel("Nucleos")
+plt.yscale('log')
+plt.show()
+fig=plt.figure()
 
+print(np.sum(A1))
+time=np.arange(0,len(A2)*dt,dt)
 
+y2=(N2*np.exp(-time*Gamma))
+plt.plot(time,ENE2,ds="steps",label="Actividad")
+plt.plot(time,y2,label="Ley de decaimiento")
+plt.title("Comparación para N=100")
+plt.xlabel("Tiempo")
+plt.legend(loc=0)
+plt.ylabel("Nucleos")
+plt.yscale('log')
+plt.show()
+fig=plt.figure()
 
+time=np.arange(0,len(A3)*dt,dt)
+
+y2=(N3*np.exp(-time*Gamma))
+plt.plot(time,ENE3,ds="steps",label="Actividad")
+plt.plot(time,y2,label="Ley de decaimiento")
+plt.title("Comparación para N=1000")
+plt.xlabel("Tiempo")
+plt.legend(loc=0)
+plt.ylabel("Nucleos")
+plt.yscale('log')
+plt.show()
+
+fig=plt.figure()
+
+time=np.arange(0,len(A4)*dt,dt)
+
+y2=(N4*np.exp(-time*Gamma))
+plt.plot(time,ENE4,ds="steps",label="Actividad")
+plt.plot(time,y2,label="Ley de decaimiento")
+plt.title("Comparación para N=10^6")
+plt.xlabel("Tiempo")
+plt.legend(loc=0)
+plt.ylabel("Nucleos")
+plt.yscale('log')
+plt.show()
