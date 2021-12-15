57775777386c594e6faf8e7269fa50acc0ee65f1
esquintero <esquintero@unal.edu.co>
2021-12-15 18:57:19 -0500

    Simulación de Poisson

--- sim3.m
+++ sim3.m
@@ -10,10 +10,28 @@ num_intervalos = round(tf/dt);
 timegrid = (1:num_intervalos) * dt;
 
 
-[inicial intermedio] = ProbPoisson(Gamma,N0,dt) %Cálculo de la prob de poisson para 1 a N(t=0) núcleos
+%[inicial intermedio] = ProbPoisson(Gamma,N0,dt); %Cálculo de la prob de poisson para 1 a N(t=0) núcleos
 
-Finicial = DistAcum(N0,inicial);
-Fintermedio = DistAcum(N0,intermedio);
+%Finicial = DistAcum(N0,inicial);
+%Fintermedio = DistAcum(N0,intermedio);
+
+N = zeros(length(timegrid),1);
+FrecPoisson = zeros(length(timegrid),1);
+Nt=N0;
+for i = timegrid
+  prob = DistPoisson(Nt, Gamma, i);
+  Fprob = DistAcum(Nt,prob);
+  r = random('uniform',0,max(Fprob));
+  for j = 1:Nt
+    if Fprob(j) < r && r < Fprob(j+1)
+      FrecPoisson(i) = FrecPoisson(i) + 1;
+    end
+  end
+  Nt = Nt - FrecPoisson(i);
+  N(i) = Nt;
+end
+
+  
 
 
 %plot(1:N0, y, '.', 'MarkerSize', 20)
@@ -40,15 +58,21 @@ function [prob_ini prob_inter] = ProbPoisson(Gamma,N0,dt)
   legend('Tiempo Inicial', 'Tiempo Intermedio');
 end
 
-function Facum = DistAcum(N0,densidad)
+function Facum = DistAcum(Nt,densidad)
   Facum = zeros(length(densidad),1);
   Facum(1) = densidad(1);
-  for i = 1:N0
+  for i = 1:Nt
     Facum(i+1) =  Facum(i) + densidad(i+1)
   end
 end
-  
-  
 
-  
+function prob = DistPoisson(Nt, Gamma, dt)
+  mu = Nt*Gamma*dt
+  prob = zeros(Nt,1);
+  for i = 1:Nt
+    prob(i) = (mu^(i) * exp(-mu))/factorial(i);
+  end
+  prob = [exp(-mu); prob];
+end
+
   
